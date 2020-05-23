b6351ae1a46f33d424034719ecd43f37fe613242
jfhovinne <git@hovinne.com>
2020-05-23 16:08:06 +0200

    Load dataset in memory for parsing, better apply defaults

--- gnrt.py
+++ gnrt.py
@@ -13,20 +13,22 @@ with open('config.yml', 'r') as yml:
 
 env = Environment(loader=FileSystemLoader('templates'))
 default_template = env.get_template(defaults['template'])
-
 dataset = {}
 
 # First iteration: get items metadata and generate dataset
 for path in Path('content').rglob('*.md'):
-
     # Get frontmatter metadata
     item = frontmatter.load(path.resolve())
     metadata = {**defaults, **item.metadata}
+    metadata['source'] = str(path)
+    metadata['target'] = str(path).replace('content/', 'public/').replace('.md', '.html')
+    metadata['link'] = metadata['target'].replace('public/', '/')
+    metadata['content'] = item.content
 
-    # Compute item link and store item in categories dataset
-    metadata['link'] = str(path).replace('content/', '/').replace('.md', '.html')
     if 'id' not in metadata:
         metadata['id'] = metadata['link']
+    if 'title' not in metadata:
+        metadata['title'] = path.stem
     dataset[metadata['id']] = metadata
 
 # Generate includes (lists) from dataset
@@ -43,29 +45,24 @@ for key, value in config['lists'].items():
     Path('includes/' + key + '.html').write_text(render)
 
 # Second iteration: render and write items
-for path in Path('content').rglob('*.md'):
-
-    # Get frontmatter metadata and body content
-    item = frontmatter.load(path.resolve())
-    metadata = {**defaults, **item.metadata}
-
+for key, item in dataset.items():
     # Get included content
-    for meta in metadata:
-        if str(metadata[meta]).startswith('includes/'):
-            metadata[meta] = Path(metadata[meta]).read_text()
+    for meta in item:
+        if str(item[meta]).startswith('includes/'):
+            item[meta] = Path(item[meta]).read_text()
 
     # Render body
-    template = Template(item.content)
-    body = template.render(metadata, config=config, data=dataset)
-    metadata['body'] = markdown2.markdown(body)
+    template = Template(item['content'])
+    body = template.render(item, config=config, data=dataset)
+    item['body'] = markdown2.markdown(body)
 
     # Render item
     template = default_template
-    if 'template' in metadata:
-        template = env.get_template(metadata['template'])
-    render = template.render(metadata, config=config, data=dataset)
+    if 'template' in item:
+        template = env.get_template(item['template'])
+    render = template.render(item, config=config, data=dataset)
 
     # Write item
-    out = Path(str(path).replace('content/', 'public/').replace('.md', '.html'))
+    out = Path(item['target'])
     Path(out.parent).mkdir(parents=True, exist_ok=True)
     out.write_text(render)
