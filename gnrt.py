4dbcf2756cbd053b915e3162996500742233d9b8
jfhovinne <git@hovinne.com>
2020-05-30 19:34:30 +0200

    Refactor

--- gnrt.py
+++ gnrt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+gnrt is a lightweight static site generator.
+"""
 
 from pathlib import Path
 import itertools
@@ -7,70 +10,84 @@ import markdown2
 import yaml
 from jinja2 import Environment, FileSystemLoader, Template
 
-with open('config.yml', 'r') as yml:
-    config = yaml.load(yml, Loader=yaml.SafeLoader)
+def load_dataset(config):
+    # First iteration: get items metadata and generate dataset
+    dataset = {}
     defaults = config['defaults']
+    for path in Path('content').rglob('*.md'):
+        # Get frontmatter metadata
+        item = frontmatter.load(path.resolve())
+        metadata = {**defaults, **item.metadata}
+        metadata['source'] = str(path)
+        if 'target' not in metadata:
+            metadata['target'] = str(path).replace('content/', 'public/').replace('.md', '.html')
+        if 'link' not in metadata:
+            metadata['link'] = metadata['target'].replace('public/', '/')
+        if 'id' not in metadata:
+            metadata['id'] = metadata['link']
+        if 'title' not in metadata:
+            metadata['title'] = path.stem
+        metadata['content'] = item.content
+        dataset[metadata['id']] = metadata
+    return dataset
 
-env = Environment(loader=FileSystemLoader('templates'))
-default_template = env.get_template(defaults['template'])
-dataset = {}
+def generate_lists(config, env, dataset):
+    # Generate includes (lists) from dataset
+    for key, value in config['lists'].items():
+        template = env.get_template(value['template'])
+        items = dataset.items()
+        if 'filter' in value:
+            items = {k: v for (k, v) in dataset.items() if v[value['filter']['key']] == value['filter']['value']}.items()
+        if 'sort' in value:
+            items = sorted(items, key=lambda x: x[1][value['sort']], reverse=value['reverse'])
+        if 'limit' in value:
+            items = dict(itertools.islice(items, value['limit'])).items()
+        render = template.render(list=value, items=items, config=config, data=dataset)
+        if 'target' in value:
+            Path(value['target']).write_text(render)
+        else:
+            Path('includes/' + key + '.html').write_text(render)
 
-# First iteration: get items metadata and generate dataset
-for path in Path('content').rglob('*.md'):
-    # Get frontmatter metadata
-    item = frontmatter.load(path.resolve())
-    metadata = {**defaults, **item.metadata}
-    metadata['source'] = str(path)
-    if 'target' not in metadata:
-        metadata['target'] = str(path).replace('content/', 'public/').replace('.md', '.html')
-    if 'link' not in metadata:
-        metadata['link'] = metadata['target'].replace('public/', '/')
-    if 'id' not in metadata:
-        metadata['id'] = metadata['link']
-    if 'title' not in metadata:
-        metadata['title'] = path.stem
-    metadata['content'] = item.content
-    dataset[metadata['id']] = metadata
+def generate_items(config, env, dataset):
+    # Second iteration: render and write items
+    for key, item in dataset.items():
+        # Get included content
+        for meta in item:
+            if str(item[meta]).startswith('includes/'):
+                item[meta] = Path(item[meta]).read_text()
 
-# Generate includes (lists) from dataset
-for key, value in config['lists'].items():
-    template = env.get_template(value['template'])
-    items = dataset.items()
-    if 'filter' in value:
-        items = {k: v for (k, v) in dataset.items() if v[value['filter']['key']] == value['filter']['value']}.items()
-    if 'sort' in value:
-        items = sorted(items, key=lambda x: x[1][value['sort']], reverse=value['reverse'])
-    if 'limit' in value:
-        items = dict(itertools.islice(items, value['limit'])).items()
-    render = template.render(list=value, items=items, config=config, data=dataset)
-    if 'target' in value:
-        Path(value['target']).write_text(render)
+        # Render body
+        template = Template(item['content'])
+        body = template.render(item, config=config, data=dataset)
+        item['body'] = markdown2.markdown(body)
+
+        # Render item
+        if 'template' in item:
+            template = env.get_template(item['template'])
+        elif 'template' in config['defaults']:
+            template = env.get_template(config['defaults']['template'])
+        else:
+            template = Template("{{ body }}")
+        render = template.render(item, config=config, data=dataset)
+
+        # Write item
+        out = Path(item['target'])
+        Path(out.parent).mkdir(parents=True, exist_ok=True)
+        out.write_text(render)
+
+def main():
+    config_file = Path('config.yml')
+    if config_file.is_file():
+        with open('config.yml', 'r') as yml:
+            config = yaml.load(yml, Loader=yaml.SafeLoader)
     else:
-        Path('includes/' + key + '.html').write_text(render)
+        config = {'defaults': {}, 'lists': {}}
+    env = Environment(loader=FileSystemLoader('templates'))
+    dataset = load_dataset(config)
+    generate_lists(config, env, dataset)
+    print("Generated %d includes" % len(config['lists']))
+    generate_items(config, env, dataset)
+    print("Generated %d targets" % len(dataset))
 
-print("Generated %d includes" % len(config['lists']))
-
-# Second iteration: render and write items
-for key, item in dataset.items():
-    # Get included content
-    for meta in item:
-        if str(item[meta]).startswith('includes/'):
-            item[meta] = Path(item[meta]).read_text()
-
-    # Render body
-    template = Template(item['content'])
-    body = template.render(item, config=config, data=dataset)
-    item['body'] = markdown2.markdown(body)
-
-    # Render item
-    template = default_template
-    if 'template' in item:
-        template = env.get_template(item['template'])
-    render = template.render(item, config=config, data=dataset)
-
-    # Write item
-    out = Path(item['target'])
-    Path(out.parent).mkdir(parents=True, exist_ok=True)
-    out.write_text(render)
-
-print("Generated %d targets" % len(dataset))
+if __name__ == '__main__':
+    main()
