b30224533422bd12e4c8c9798f999ac95081d5ba
jfhovinne <git@hovinne.com>
2022-03-20 17:42:53 +0100

    0.3.0 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='gnrt',
-    version='0.2.0',
+    version='0.3.0',
     author='jfhovinne',
     author_email='gnrt@hovinne.com',
     license='MIT',
